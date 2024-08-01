7ba32040d3b3401a7676b041a58ebd6658cb1e5f
Adnangad <gardobuyaadnan@gmail.com>
2024-08-01 18:08:25 +0300

    3-connect to db-fixed errors

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -52,22 +52,13 @@ def get_logger() -> logging.Logger:
 
 def get_db():
     """Establishes connection to the db"""
-    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
-    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
-    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
-    database = os.getenv('PERSONAL_DATA_DB_NAME')
-    try:
-        connection = mysql.connector.connect(
-            user=username,
-            password=password,
-            host=host,
-            database=database
-        )
-        if connection.is_connected():
-            return connection
-    except Error as e:
-        print(f"Error: {e}")
-        return None
+    my_db = mysql.connector.connect(
+        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=os.getenv('PERSONAL_DATA_DB_NAME')
+    )
+    return my_db
 
 
 def main() -> None:
