c9d5120296a6e20e1d7f2a795b7d25f39bc8b19b
Adnangad <gardobuyaadnan@gmail.com>
2024-08-01 17:41:34 +0300

    3-connect to db

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,7 @@ from typing import List
 import logging
 import os
 import mysql.connector
+from mysql.connector import Error
 
 
 class RedactingFormatter(logging.Formatter):
@@ -49,32 +50,21 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> mysql.connector.connection.MYSQLConnection:
-    """ Establishes a connection to the db"""
-    mydb = mysql.connector.connect(
-        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
-        database=os.getenv('PERSONAL_DATA_DB_NAME'),
-        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
-        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+def get_db():
+    """Establishes connection to the db"""
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    database = os.getenv('PERSONAL_DATA_DB_NAME')
+    try:
+        connection = mysql.connector.connect(
+            user=username,
+            password=password,
+            host=host,
+            database=database
         )
-    return mydb
-
-
-def main():
-    """main function"""
-    db_con = get_db()
-    db_connect = db_con.cursor()
-    db_connect.execute("SELECT * FROM users;")
-    fieldz = [field[0] for field in db_connect.description]
-    logger = get_logger()
-    for row in db_connect:
-        result = ''
-        for i, j in zip(row, fieldz):
-            result += f'{i}={(j)}; '
-        logger.info(result)
-    db_connect.close()
-    db_con.close()
-
-
-if __name__ == '__main__':
-    main()
+        if connection.is_connected():
+            return connection
+    except Error as e:
+        print(f"Error: {e}")
+        return None
