7160cfea2341058dcad6b59fb52db64842266272
Adnangad <gardobuyaadnan@gmail.com>
2024-08-05 15:38:57 +0300

    2-frobidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,12 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauth() -> None:
   """Returns an abort"""
   abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbiden() -> None:
+  abort(403)
