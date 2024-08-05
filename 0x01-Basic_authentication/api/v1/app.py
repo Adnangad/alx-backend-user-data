b5d36410cdaf42bb8ba8a8a2b160114692015e65
Adnangad <gardobuyaadnan@gmail.com>
2024-08-05 18:35:32 +0300

    5-auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,26 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+auth = os.getenv('AUTH_TYPE')
+if auth:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def before_req():
+    """A method that defines what happens bfre a req
+    """
+    if auth is None:
+        return
+    ls = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if auth.require_auth(request.path, ls):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 @app.errorhandler(404)
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,11 +8,27 @@ class Auth:
     """ Manages API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """For now returns a False bool"""
-        return False
+        if path is None:
+            return True
+        if not path.endswith('/'):
+            path = path + '/'
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for x in excluded_paths:
+            if x[-1] != '/':
+                x += '/'
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """Returns a str"""
-        return None
+        if request is None:
+            return None
+        if request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
