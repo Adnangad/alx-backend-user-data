d15502dc8082f9dccfaa3dd27ed705daeae2036b
Adnangad <gardobuyaadnan@gmail.com>
2024-08-13 17:43:53 +0300

    added a logout route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -51,7 +51,7 @@ def logout():
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
-        redirect(url_for('/'))
+        return redirect(url_for('index'))
     else:
         abort(403)
 
