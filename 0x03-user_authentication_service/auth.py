1019830ce1cfc09ddaf32207dc2c165d419d1249
Adnangad <gardobuyaadnan@gmail.com>
2024-08-13 17:31:41 +0300

    added a method that destroys a users session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -71,3 +71,8 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroys a users session_id by 
+        setting it to none"""
+        self._db.update_user(user_id, session_id=None)
